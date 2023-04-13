bcf83bb550d1aafea9410464c679237be6faa3fc
spytech-app <spytechapp@gmail.com>
2023-04-13 12:14:56 -0300

    fix: deploy

---
+++ precache-manifest.1302d790462e228ca53759b26a27c071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204db8680e81943877d7989dfeb083c5",
+    "url": "/calculadora-build/index.html"
+  },
+  {
+    "revision": "dd35900d7862ef12969b",
+    "url": "/calculadora-build/static/css/main.da94598a.chunk.css"
+  },
+  {
+    "revision": "d4d6a9ae3b5ca7770b28",
+    "url": "/calculadora-build/static/js/2.aa42a96f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/calculadora-build/static/js/2.aa42a96f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd35900d7862ef12969b",
+    "url": "/calculadora-build/static/js/main.01e3bdbe.chunk.js"
+  },
+  {
+    "revision": "a3ec80e324062fc0d639",
+    "url": "/calculadora-build/static/js/runtime-main.627f4ec8.js"
+  },
+  {
+    "revision": "db7d8c3ac9681c785b772931615bc41e",
+    "url": "/calculadora-build/static/media/logo.db7d8c3a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calculadora-financeira/precache-manifest.aec932978c9e896be597b9c0f1bcdceb.js"
+  "/calculadora-build/precache-manifest.1302d790462e228ca53759b26a27c071.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculadora-financeira/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculadora-build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
